1efe496873c16983d8fc869184e74f62cb82d39d
ceranic_david <david.ceranic@nth.ch>
2024-07-08 01:12:03 +0200

    refacto rr

--- src/app/core/components/master-layout/master-layout.component.ts
+++ src/app/core/components/master-layout/master-layout.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { SidebarComponent } from '../components/sidebar/sidebar.component';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-master-layout',
